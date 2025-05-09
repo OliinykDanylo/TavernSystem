9d406fc2e2e0184e3ad892e9fb3f070cdd066c9f
danylooliinyk <danylooliinyk.dev@gmail.com>
2025-05-09 11:47:37 +0200

    src added

--- TavernSystem.Repositories/IAdventurerRepository.cs
+++
@@ -1,8 +0,0 @@
-using TavernSystem.Models;
-
-public interface IAdventurerRepository
-{
-    IEnumerable<Adventurer> GetAll();
-    Adventurer? GetById(int id);
-    void Add(Adventurer adventurer);
-}
